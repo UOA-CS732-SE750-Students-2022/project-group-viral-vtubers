783cf279f0eb3fe9e021d7072ee7cee6461070ed
ff-dev-45 <flynn.fromont@gmail.com>
2022-04-20 22:17:11 +1200

    Setup basic firebase auth service

--- viral-vtubers-frontend/src/app/app.module.ts
+++ viral-vtubers-frontend/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { UserProfileComponent } from './user-profile/user-profile.component';
@@ -10,11 +11,19 @@ import { ActivityFeedComponent } from './activity-feed/activity-feed.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { environment } from 'src/environments/environment';
+import { AuthService } from './shared/auth/auth.service';
 
 @NgModule({
   declarations: [AppComponent, UserProfileComponent, CartComponent, ActivityFeedComponent, SignInComponent, SignUpComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, CommonModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+  ],
+  providers: [AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
