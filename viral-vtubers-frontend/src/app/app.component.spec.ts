7dbc806fbdd550142e2bad172c87e3cddc8d42e1
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-04-22 04:16:47 +1200

    remove obsolete test for app

--- viral-vtubers-frontend/src/app/app.component.spec.ts
+++ viral-vtubers-frontend/src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
+import { routes } from './app-routing.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -23,12 +24,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('viral-vtubers-frontend');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'viral-vtubers-frontend app is running!'
-    );
-  });
 });
