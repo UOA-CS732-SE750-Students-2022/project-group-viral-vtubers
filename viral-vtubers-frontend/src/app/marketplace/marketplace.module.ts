a1d3e5cb4ea75848dc60e9442971e017580a46f1
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-04-22 02:56:40 +1200

    add products module components routes

---
+++ viral-vtubers-frontend/src/app/marketplace/marketplace.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CreateProductComponent } from './create-product/create-product.component';
+
+const routes: Routes = [
+  {
+    path: 'product/:id',
+    component: ProductDetailsComponent,
+  },
+  {
+    path: 'create-product',
+    component: CreateProductComponent,
+  },
+  {
+    path: '',
+    component: ProductsComponent,
+  }
+];
+
+@NgModule({
+  declarations: [
+    ProductsComponent,
+    ProductDetailsComponent,
+    CreateProductComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MarketplaceModule { }
