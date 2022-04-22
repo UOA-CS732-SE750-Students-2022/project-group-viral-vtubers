9cb26ca20259a465b1824a337d0ea596ad71b352
Hiruna Jayamanne <hirunav2@gmail.com>
2022-04-22 23:39:51 +1200

    Add MongoDB support

--- viral-vtubers-backend/src/main/kotlin/com/viralvtubers/Application.kt
+++ viral-vtubers-backend/src/main/kotlin/com/viralvtubers/Application.kt
@@ -1,10 +1,13 @@
 package com.viralvtubers
 
+import com.viralvtubers.database.mongo.MongoDatabase
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.viralvtubers.plugins.*
 
 fun main() {
+    val database = MongoDatabase()
+
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
         configureSecurity()
