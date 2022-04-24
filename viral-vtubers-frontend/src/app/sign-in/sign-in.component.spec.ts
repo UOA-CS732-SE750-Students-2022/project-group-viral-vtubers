801db7bcf7ff8b8e49a3de41467aa0017b74c38a
ff-dev-45 <flynn.fromont@gmail.com>
2022-04-25 02:34:21 +1200

    Setup basic signin page elements

--- viral-vtubers-frontend/src/app/sign-in/sign-in.component.spec.ts
+++ viral-vtubers-frontend/src/app/sign-in/sign-in.component.spec.ts
@@ -8,9 +8,8 @@ describe('SignInComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignInComponent ]
-    })
-    .compileComponents();
+      declarations: [SignInComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
