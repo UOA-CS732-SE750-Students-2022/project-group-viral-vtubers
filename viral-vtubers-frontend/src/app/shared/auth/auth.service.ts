22f452eda4433c5d267a741efefa9fd10de0a186
ffro805 <66896513+ff-dev-45@users.noreply.github.com>
2022-04-26 00:31:46 +1200

    Update viral-vtubers-frontend/src/app/shared/auth/auth.service.ts
    Co-authored-by: ZHENK <chengzhenyang@gmail.com>

--- viral-vtubers-frontend/src/app/shared/auth/auth.service.ts
+++ viral-vtubers-frontend/src/app/shared/auth/auth.service.ts
@@ -79,7 +79,7 @@ export class AuthService {
       });
   }
 
-  // Returns true when user is looged in and email is verified
+  // Returns true when user is logged in and email is verified
   get isLoggedIn(): boolean {
     const user = JSON.parse(localStorage.getItem('user')!);
     return user !== null && user.emailVerified !== false ? true : false;
