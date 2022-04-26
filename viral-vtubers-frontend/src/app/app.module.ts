f7267ea1da89f0708b5880e28d222c609f9c388b
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-04-27 00:44:11 +1200

    g c footer

--- viral-vtubers-frontend/src/app/app.module.ts
+++ viral-vtubers-frontend/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { VerifyEmailComponent } from './verify-email/verify-email.component';
 import { SharedModule } from './shared/shared.module';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { SharedModule } from './shared/shared.module';
     NotFoundComponent,
     ForgotPasswordComponent,
     VerifyEmailComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
