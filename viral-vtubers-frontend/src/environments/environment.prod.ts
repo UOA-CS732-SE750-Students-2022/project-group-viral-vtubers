f285164a0cfd2b2ae9d270901bf0da4d4bca0104
ffro805 <66896513+ff-dev-45@users.noreply.github.com>
2022-04-27 01:06:20 +1200

    Fixing firebase auth error on prod (#36)
    * temporary fix, adding firebase config details to prod environment
    
    * Increase angular budget for building site

--- viral-vtubers-frontend/src/environments/environment.prod.ts
+++ viral-vtubers-frontend/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCrODvjXtq08KnhY1rYkczKgzIZXn5C0Yo',
+    authDomain: 'viral-vtubers.firebaseapp.com',
+    projectId: 'viral-vtubers',
+    storageBucket: 'viral-vtubers.appspot.com',
+    messagingSenderId: '102572388853',
+    appId: '1:102572388853:web:07234c02be6aa969e030bc',
+  },
 };
