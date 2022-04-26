51fd7d87aaf2be93f97ce953d7c6671fdaa28eef
ZHENK <chengzhenyang@gmail.com>
2022-04-26 20:23:11 +1200

    Implement Navbar and _variables.scss (#33)
    * Implement Navbar and _variables.scss
    
    * Fix spelling in Search placeholder
    
    * update styling for navbar
    
    * fix things
    
    Co-authored-by: lin8231 <62003343+lin8231@users.noreply.github.com>

--- viral-vtubers-frontend/src/app/shared/shared.module.ts
+++ viral-vtubers-frontend/src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { VrmViewerComponent } from './vrm-viewer/vrm-viewer.component';
 
 @NgModule({
-  declarations: [VrmViewerComponent],
+  declarations: [VrmViewerComponent, NavbarComponent],
   imports: [CommonModule],
+  exports: [NavbarComponent],
 })
 export class SharedModule {}
