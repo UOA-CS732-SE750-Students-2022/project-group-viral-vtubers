0038f145b586b8b6f6e95fe2384e342678bdaa70
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-04-28 00:10:05 +1200

    add a few more mails

--- viral-vtubers-frontend/src/app/mail/inbox/inbox.component.ts
+++ viral-vtubers-frontend/src/app/mail/inbox/inbox.component.ts
@@ -14,16 +14,37 @@ export class InboxComponent implements OnInit {
       {
         body: 'Im probably a year late but still want to ask, what was your independent variable for your physics IA Im also doing mine on rotational kinetic energy down a slop and want to make sure im not doing something some else has done before \nMany thanks!',
         date: '2020-06-01T00:00:00.000Z',
-        id: 'fdjsio12',
+        id: 'mail0',
         read: false,
         title: 'Physics IA questions',
       },
       {
         body: "Hey, I was going through the IBO subreddit and I saw that your Physics IA was about rotational kinetic energy on a ramp and I'm working on something similar. I was just wondering if you could send me your IA to use as an exemplar since you got a 22. Any help would be appreciated, thanks",
         date: '2020-06-01T00:00:00.000Z',
-        id: 'afdjsidsao12',
-        read: false,
+        id: 'mail1',
+        read: true,
         title: 'Physics IA:',
+      },
+      {
+        body: 'Hello! You are receiving this message because you have followed a user profile in the past.\nStarting on 08/19/2019, we will begin showing some users new followers of their profile. In about 3 months, all users will be able to see all the usernames of their followers, including follows that were done in the past, while the user profile feature was in beta. Please take a moment to check your subscriptions list (where followed users also appear) to ensure that if you follow someone, you are comfortable with them being aware of this.',
+        date: '2019-06-01T00:00:00.000Z',
+        id: 'mail2',
+        read: true,
+        title: 'User Profile Transparency Update:',
+      },
+      {
+        body: "Hey, I was going through the IBO subreddit and I saw that your Physics IA was about rotational kinetic energy on a ramp and I'm working on something similar. I was just wondering if you could send me your IA to use as an exemplar since you got a 22. Any help would be appreciated, thanks",
+        date: '2020-06-01T00:00:00.000Z',
+        id: 'mail3',
+        read: true,
+        title: 'Physics IA:',
+      },
+      {
+        body: 'Hello! You are receiving this message because you have followed a user profile in the past.\nStarting on 08/19/2019, we will begin showing some users new followers of their profile. In about 3 months, all users will be able to see all the usernames of their followers, including follows that were done in the past, while the user profile feature was in beta. Please take a moment to check your subscriptions list (where followed users also appear) to ensure that if you follow someone, you are comfortable with them being aware of this.',
+        date: '2019-06-01T00:00:00.000Z',
+        id: 'mail4',
+        read: true,
+        title: 'User Profile Transparency Update:',
       },
     ].map((mail) => ({ ...mail, date: moment(mail.date).fromNow() }));
   }
