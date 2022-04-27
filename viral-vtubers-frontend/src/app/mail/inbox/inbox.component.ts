43200b83819746d5128cb098a04cb985fc7a6867
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-04-27 23:32:42 +1200

    get some dummy data

--- viral-vtubers-frontend/src/app/mail/inbox/inbox.component.ts
+++ viral-vtubers-frontend/src/app/mail/inbox/inbox.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Mail } from 'src/schema/type';
 
 @Component({
   selector: 'app-inbox',
   templateUrl: './inbox.component.html',
-  styleUrls: ['./inbox.component.scss']
+  styleUrls: ['./inbox.component.scss'],
 })
 export class InboxComponent implements OnInit {
+  mails: Mail[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.mails = [
+      {
+        body: 'Im probably a year late but still want to ask, what was your independent variable for your physics IA Im also doing mine on rotational kinetic energy down a slop and want to make sure im not doing something some else has done before \nMany thanks!',
+        date: '2020-06-01T00:00:00.000Z',
+        id: 'fdjsio12',
+        read: false,
+        title: 'Physics IA questions',
+      },
+      {
+        body: "Hey, I was going through the IBO subreddit and I saw that your Physics IA was about rotational kinetic energy on a ramp and I'm working on something similar. I was just wondering if you could send me your IA to use as an exemplar since you got a 22. Any help would be appreciated, thanks",
+        date: '2020-06-01T00:00:00.000Z',
+        id: 'afdjsidsao12',
+        read: false,
+        title: 'Physics IA:',
+      },
+    ];
   }
 
+  ngOnInit(): void {}
 }
