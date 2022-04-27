4acc88fee373009b312f184437166b287897a28a
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-04-28 02:34:39 +1200

    wow we actually used good practice

--- viral-vtubers-frontend/src/app/mail/mail.component.ts
+++ viral-vtubers-frontend/src/app/mail/mail.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mail',
   templateUrl: './mail.component.html',
-  styleUrls: ['./mail.component.scss']
+  styleUrls: ['./mail.component.scss'],
 })
 export class MailComponent implements OnInit {
+  activeRoute: string = this.router.url;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.router.events.subscribe((res) => {
+      this.activeRoute = this.router.url;
+    });
   }
 
+  ngOnInit(): void {}
 }
