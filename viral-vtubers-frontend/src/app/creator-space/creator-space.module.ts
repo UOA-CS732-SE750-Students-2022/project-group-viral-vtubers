839fcbe83f8ee829ddd194de5db554487739d05b
Mercury Lin <62003343+lin8231@users.noreply.github.com>
2022-04-30 13:23:24 +1200

    Revert "Implement Create product page (#77)"
    This reverts commit dfeb18fe2d9b29c5d05cc2a66c077abfac55ccdd.

--- viral-vtubers-frontend/src/app/creator-space/creator-space.module.ts
+++ viral-vtubers-frontend/src/app/creator-space/creator-space.module.ts
@@ -1,8 +1,6 @@
-import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SharedModule } from '../shared/shared.module';
 
 import { CreateProductComponent } from './create-product/create-product.component';
 import { CreatorSpaceComponent } from './creator-space.component';
@@ -22,11 +20,6 @@ export const routes: Routes = [
 
 @NgModule({
   declarations: [CreateProductComponent, CreatorSpaceComponent],
-  imports: [
-    CommonModule,
-    SharedModule,
-    DragDropModule,
-    RouterModule.forChild(routes),
-  ],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class CreatorSpaceModule {}
