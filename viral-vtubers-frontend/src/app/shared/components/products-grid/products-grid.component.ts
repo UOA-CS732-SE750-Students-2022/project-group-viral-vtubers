8e3037254f1653c225c827331d6a6b0be6feb732
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-05-11 00:16:34 +1200

    woahhh it works

--- viral-vtubers-frontend/src/app/shared/components/products-grid/products-grid.component.ts
+++ viral-vtubers-frontend/src/app/shared/components/products-grid/products-grid.component.ts
@@ -8,56 +8,7 @@ import { ProductBlurbFragmentFragment } from 'src/schema/type';
 })
 export class ProductsGridComponent implements OnInit {
   @Input()
-  public products: Array<ProductBlurbFragmentFragment> = [
-    {
-      id: '1',
-      name: 'Aya 着替B ver0.98',
-      minPrice: 12.99,
-      numLikes: 29,
-      titleImage:
-        'https://pbs.twimg.com/profile_images/950544018160709632/TBueVZZr_400x400.jpg',
-    },
-    {
-      id: '1',
-      name: 'Aya 着替B ver0.98',
-      minPrice: 12.99,
-      numLikes: 29,
-      titleImage:
-        'https://pbs.twimg.com/profile_images/890968801222590464/zy5R43Sf_400x400.jpg',
-    },
-    {
-      id: '1',
-      name: 'Aya 着替B ver0.98',
-      minPrice: 12.99,
-      numLikes: 29,
-      titleImage:
-        'https://i.pinimg.com/originals/df/c7/4b/dfc74b6867617fc8220fddb0efc9d916.jpg',
-    },
-    {
-      id: '1',
-      name: 'Aya 着替B ver0.98',
-      minPrice: 12.99,
-      numLikes: 29,
-      titleImage:
-        'https://c4.wallpaperflare.com/wallpaper/229/65/146/anime-love-live-nozomi-tojo-wallpaper-preview.jpg',
-    },
-    {
-      id: '1',
-      name: 'Aya 着替B ver0.98',
-      minPrice: 12.99,
-      numLikes: 29,
-      titleImage:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNvuGoYH3SjULe-zItrAlyS7MaJPoR0wsjRZWKHbNXsfAU0bHLrRF4j1tP93k7jyhbnqk&usqp=CAU',
-    },
-    {
-      id: '1',
-      name: 'Aya 着替B ver0.98',
-      minPrice: 12.99,
-      numLikes: 29,
-      titleImage:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPoAqbxmqSwl8qjS9Ad3BTjTsBwZCzvhJ7Jw&usqp=CAU',
-    },
-  ];
+  public products: Array<ProductBlurbFragmentFragment> = [];
 
   constructor() {}
 
