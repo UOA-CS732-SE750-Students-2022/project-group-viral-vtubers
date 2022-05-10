ebc1238ff1d46de511b47d455a0369bee5f43c20
Hiruna Jayamanne <hiru@hiru.dev>
2022-05-11 01:46:58 +1200

    Test VerifyEmailComponent

--- viral-vtubers-frontend/src/app/verify-email/verify-email.component.spec.ts
+++ viral-vtubers-frontend/src/app/verify-email/verify-email.component.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { AuthService } from '../shared/auth/auth.service';
 import { VerifyEmailComponent } from './verify-email.component';
 
 describe('VerifyEmailComponent', () => {
   let component: VerifyEmailComponent;
   let fixture: ComponentFixture<VerifyEmailComponent>;
+  let authService: AuthService;
+  let mailSent: boolean;
 
   beforeEach(async () => {
+    authService = {
+      userData: {
+        user: {
+          email: 'foo@bar',
+        },
+      },
+      SendVerificationMail: async () => {
+        mailSent = true;
+      },
+    } as AuthService;
     await TestBed.configureTestingModule({
       declarations: [VerifyEmailComponent],
+      providers: [{ provide: AuthService, useValue: authService }],
     }).compileComponents();
   });
 
@@ -20,5 +34,21 @@ describe('VerifyEmailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set email', () => {
+    const EMAIL = 'hjay473@aucklanduni.ac.nz';
+    authService.userData.email = EMAIL;
+    fixture.detectChanges();
+    expect(
+      fixture.nativeElement.querySelector('div div p strong').textContent
+    ).toContain(EMAIL);
+    expect(component).toBeTruthy();
+  });
+
+  it('should send a verification email', () => {
+    const button = fixture.nativeElement.querySelector('input[type="button"]');
+    button.click();
+    expect(mailSent).toBe(true);
   });
 });
