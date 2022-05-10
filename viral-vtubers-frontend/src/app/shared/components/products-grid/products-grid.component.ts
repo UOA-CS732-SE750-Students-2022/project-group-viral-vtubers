0059c6748089fcf04e3993d8463394e8060fc4c9
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-05-10 23:59:53 +1200

    generate products-grid shared comp

---
+++ viral-vtubers-frontend/src/app/shared/components/products-grid/products-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-grid',
+  templateUrl: './products-grid.component.html',
+  styleUrls: ['./products-grid.component.scss']
+})
+export class ProductsGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
