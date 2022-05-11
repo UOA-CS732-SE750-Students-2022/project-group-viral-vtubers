4a4bdea9c1dc909b554de1bb0e4ab5a75bb27cc6
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-05-11 22:50:36 +1200

    fjdskal finally scroll working

--- viral-vtubers-frontend/src/app/marketplace/browse-products/browse-products.component.ts
+++ viral-vtubers-frontend/src/app/marketplace/browse-products/browse-products.component.ts
@@ -75,38 +75,38 @@ export class BrowseProductsComponent implements OnInit {
         titleImage:
           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNvuGoYH3SjULe-zItrAlyS7MaJPoR0wsjRZWKHbNXsfAU0bHLrRF4j1tP93k7jyhbnqk&usqp=CAU',
       },
-      {
-        id: '1',
-        name: 'Aya 着替B ver0.98',
-        minPrice: 12.99,
-        numLikes: 29,
-        titleImage:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPoAqbxmqSwl8qjS9Ad3BTjTsBwZCzvhJ7Jw&usqp=CAU',
-      },
-      {
-        id: '1',
-        name: 'Aya 着替B ver0.98',
-        minPrice: 12.99,
-        numLikes: 29,
-        titleImage:
-          'https://64.media.tumblr.com/2c1330e52fe9a1f22729be266f37477f/tumblr_np0da4RvDz1uwhadvo3_400.jpg',
-      },
-      {
-        id: '1',
-        name: 'Aya 着替B ver0.98',
-        minPrice: 12.99,
-        numLikes: 29,
-        titleImage:
-          'https://64.media.tumblr.com/256c6a85096830310954813172b8e266/tumblr_np0da4RvDz1uwhadvo1_250.jpg',
-      },
-      {
-        id: '1',
-        name: 'Aya 着替B ver0.98',
-        minPrice: 12.99,
-        numLikes: 29,
-        titleImage:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyF3bjSJt9T-dHmFcPiRWfPx9gDCOOGCGz8LoY6AJ1snM-mJNFKZLYa7xiQCVwM42Mf-E&usqp=CAU',
-      },
+      // {
+      //   id: '1',
+      //   name: 'Aya 着替B ver0.98',
+      //   minPrice: 12.99,
+      //   numLikes: 29,
+      //   titleImage:
+      //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPoAqbxmqSwl8qjS9Ad3BTjTsBwZCzvhJ7Jw&usqp=CAU',
+      // },
+      // {
+      //   id: '1',
+      //   name: 'Aya 着替B ver0.98',
+      //   minPrice: 12.99,
+      //   numLikes: 29,
+      //   titleImage:
+      //     'https://64.media.tumblr.com/2c1330e52fe9a1f22729be266f37477f/tumblr_np0da4RvDz1uwhadvo3_400.jpg',
+      // },
+      // {
+      //   id: '1',
+      //   name: 'Aya 着替B ver0.98',
+      //   minPrice: 12.99,
+      //   numLikes: 29,
+      //   titleImage:
+      //     'https://64.media.tumblr.com/256c6a85096830310954813172b8e266/tumblr_np0da4RvDz1uwhadvo1_250.jpg',
+      // },
+      // {
+      //   id: '1',
+      //   name: 'Aya 着替B ver0.98',
+      //   minPrice: 12.99,
+      //   numLikes: 29,
+      //   titleImage:
+      //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyF3bjSJt9T-dHmFcPiRWfPx9gDCOOGCGz8LoY6AJ1snM-mJNFKZLYa7xiQCVwM42Mf-E&usqp=CAU',
+      // },
     ];
   }
 
