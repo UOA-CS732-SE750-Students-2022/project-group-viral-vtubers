d13a0dcaa8bfcdb5e6360079fed8786426574631
Mercury Lin <62003343+lin8231@users.noreply.github.com>
2022-05-11 20:15:13 +1200

    Implement Favourites page in Me module (#109)
    * add href to cart in navbar
    
    * add link to dp in navbar
    
    * add favourites component header
    
    * add 2way binding for input
    
    * add empty page figure
    
    * finish favourites page
    
    * make grid more responsive
    
    * update this to have a reasonable number of placeholders

--- viral-vtubers-frontend/src/app/me/favourites/favourites.component.ts
+++ viral-vtubers-frontend/src/app/me/favourites/favourites.component.ts
@@ -1,15 +1,93 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductBlurbFragmentFragment } from 'src/schema/type';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.component.html',
-  styleUrls: ['./favourites.component.scss']
+  styleUrls: ['./favourites.component.scss'],
 })
 export class FavouritesComponent implements OnInit {
+  sortOptions: sortBy[] = [
+    { id: 'newest', name: 'Time added (newest) ' },
+    { id: 'oldest', name: 'Time added (oldest) ' },
+  ];
+  selectedSortOption: sortBy = this.sortOptions[0];
 
-  constructor() { }
+  products: ProductBlurbFragmentFragment[];
 
-  ngOnInit(): void {
+  constructor() {
+    this.products = [
+      {
+        id: '1',
+        name: 'Aya 着替B ver0.98',
+        minPrice: 12.99,
+        numLikes: 29,
+        titleImage:
+          'https://pbs.twimg.com/profile_images/950544018160709632/TBueVZZr_400x400.jpg',
+      },
+      {
+        id: '1',
+        name: 'Aya 着替B ver0.98',
+        minPrice: 12.99,
+        numLikes: 29,
+        titleImage:
+          'https://pbs.twimg.com/profile_images/950544018160709632/TBueVZZr_400x400.jpg',
+      },
+      {
+        id: '1',
+        name: 'Aya 着替B ver0.98',
+        minPrice: 12.99,
+        numLikes: 29,
+        titleImage:
+          'https://pbs.twimg.com/profile_images/950544018160709632/TBueVZZr_400x400.jpg',
+      },
+      {
+        id: '1',
+        name: 'Aya 着替B ver0.98',
+        minPrice: 12.99,
+        numLikes: 29,
+        titleImage:
+          'https://pbs.twimg.com/profile_images/950544018160709632/TBueVZZr_400x400.jpg',
+      },
+      {
+        id: '1',
+        name: 'Aya 着替B ver0.98',
+        minPrice: 12.99,
+        numLikes: 29,
+        titleImage:
+          'https://pbs.twimg.com/profile_images/950544018160709632/TBueVZZr_400x400.jpg',
+      },
+      {
+        id: '1',
+        name: 'Aya 着替B ver0.98',
+        minPrice: 12.99,
+        numLikes: 29,
+        titleImage:
+          'https://pbs.twimg.com/profile_images/950544018160709632/TBueVZZr_400x400.jpg',
+      },
+      {
+        id: '1',
+        name: 'Aya 着替B ver0.98',
+        minPrice: 12.99,
+        numLikes: 29,
+        titleImage:
+          'https://pbs.twimg.com/profile_images/950544018160709632/TBueVZZr_400x400.jpg',
+      },
+      {
+        id: '1',
+        name: 'Aya 着替B ver0.98',
+        minPrice: 12.99,
+        numLikes: 29,
+        titleImage:
+          'https://pbs.twimg.com/profile_images/950544018160709632/TBueVZZr_400x400.jpg',
+      },
+    ];
   }
 
+  ngOnInit(): void {}
+}
+
+interface sortBy {
+  id: string;
+  name: string;
 }
