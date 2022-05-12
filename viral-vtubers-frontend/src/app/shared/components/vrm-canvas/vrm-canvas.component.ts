eeee02cc4a9315988c905e493c2fc1dd7d748d8e
ZHENK <chengzhenyang@gmail.com>
2022-05-12 19:12:17 +1200

    Setup vrm canvas

---
+++ viral-vtubers-frontend/src/app/shared/components/vrm-canvas/vrm-canvas.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vrm-canvas',
+  templateUrl: './vrm-canvas.component.html',
+  styleUrls: ['./vrm-canvas.component.scss'],
+})
+export class VrmCanvasComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- viral-vtubers-frontend/src/app/shared/shared.module.ts
+++ viral-vtubers-frontend/src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NgtCanvasModule } from '@angular-three/core';
 
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductsGridComponent } from './components/products-grid/products-grid.component';
 import { TagsComponent } from './components/tags/tags.component';
+import { VrmCanvasComponent } from './components/vrm-canvas/vrm-canvas.component';
 import { VrmViewerComponent } from './vrm-viewer/vrm-viewer.component';
 
 @NgModule({
@@ -14,8 +16,9 @@ import { VrmViewerComponent } from './vrm-viewer/vrm-viewer.component';
     NavbarComponent,
     TagsComponent,
     ProductsGridComponent,
+    VrmCanvasComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, NgtCanvasModule],
   exports: [
     VrmViewerComponent,
     NavbarComponent,
