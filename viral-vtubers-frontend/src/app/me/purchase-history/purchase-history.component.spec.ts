b927d895e9ac72e69f976f90635ad6294fc3ae88
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-05-12 18:14:13 +1200

    add purchase history component

---
+++ viral-vtubers-frontend/src/app/me/purchase-history/purchase-history.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PurchaseHistoryComponent } from './purchase-history.component';
+
+describe('PurchaseHistoryComponent', () => {
+  let component: PurchaseHistoryComponent;
+  let fixture: ComponentFixture<PurchaseHistoryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PurchaseHistoryComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PurchaseHistoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ viral-vtubers-frontend/src/app/me/purchase-history/purchase-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase-history',
+  templateUrl: './purchase-history.component.html',
+  styleUrls: ['./purchase-history.component.scss']
+})
+export class PurchaseHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
