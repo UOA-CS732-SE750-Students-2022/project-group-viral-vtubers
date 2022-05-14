00583d80942ee7109feb131d96fc33646661ff2e
ff-dev-45 <flynn.fromont@gmail.com>
2022-05-15 03:46:28 +1200

    added in overlay

--- viral-vtubers-frontend/src/app/commissions/view-requests/view-requests.component.ts
+++ viral-vtubers-frontend/src/app/commissions/view-requests/view-requests.component.ts
@@ -1,24 +1,53 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { firstValueFrom, Observable } from 'rxjs';
 import { OrderService } from 'src/app/services/order.service';
-import { OrderPaginationFragmentFragment } from 'src/schema/type';
+import {
+  OrderFragmentFragment,
+  OrderPaginationFragmentFragment,
+} from 'src/schema/type';
 
 @Component({
   selector: 'app-view-requests',
   templateUrl: './view-requests.component.html',
   styleUrls: ['./view-requests.component.scss'],
+  animations: [
+    trigger('inOutAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.2s ease', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('0.2s ease', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class ViewRequestsComponent implements OnInit {
-  public requests$: Observable<OrderPaginationFragmentFragment>;
-  constructor(private orderService: OrderService) {
+  requests$: Observable<OrderPaginationFragmentFragment>;
+
+  selectedOrder?: OrderFragmentFragment;
+  constructor(private router: Router, private orderService: OrderService) {
     this.requests$ = orderService.getAllOrders().allOrders$;
   }
 
   ngOnInit(): void {}
 
-  apply(event: Event, commissionId: string): void {
+  navigateToUser(id: string) {
+    this.router.navigateByUrl('/user/' + id);
+  }
+
+  setSelectedOrder(order?: OrderFragmentFragment) {
+    this.selectedOrder = order;
+  }
+
+  async apply(event: Event, commissionId: string): Promise<void> {
     event.preventDefault();
-    this.orderService.applyOrder(commissionId).subscribe();
+    await firstValueFrom(this.orderService.applyOrder(commissionId));
+    this.selectedOrder = undefined;
+    alert('Thank you for Applying, pls wait');
   }
 
   preventDefault(event: Event): void {
