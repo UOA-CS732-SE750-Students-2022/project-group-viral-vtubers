dde5e03b817d96bfd5f7109f7eb1e5ce87224b9b
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-05-15 04:30:21 +1200

    Get orders with limit

--- viral-vtubers-frontend/src/app/main-page/main-page.component.ts
+++ viral-vtubers-frontend/src/app/main-page/main-page.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { map, Observable } from 'rxjs';
 import {
+  OrderPaginationFragmentFragment,
   ProductBlurbFragmentFragment,
   ProductPaginationFragmentFragment,
 } from 'src/schema/type';
+import { OrderService } from '../services/order.service';
 import { ProductService } from '../services/product.service';
 
 @Component({
@@ -15,10 +17,18 @@ import { ProductService } from '../services/product.service';
 export class MainPageComponent implements OnInit {
   products$?: Observable<ProductBlurbFragmentFragment[]>;
 
-  constructor(private router: Router, private productService: ProductService) {
+  requests$: Observable<OrderPaginationFragmentFragment>;
+
+  constructor(
+    private router: Router,
+    private productService: ProductService,
+    private orderService: OrderService
+  ) {
     this.products$ = this.productService
       .getProducts(undefined, undefined, undefined, 6)
       .products$.pipe(map((p) => p.edges.map((e) => e.node)));
+
+    this.requests$ = orderService.getOrdersWithLimit(8).newestOrders$;
   }
 
   ngOnInit(): void {}
