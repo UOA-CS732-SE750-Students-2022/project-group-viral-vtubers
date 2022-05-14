a7554dd7200b1767067d0b2b7d68cd00f6309582
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-05-15 04:16:57 +1200

    connect products on main page

--- viral-vtubers-frontend/src/app/main-page/main-page.component.ts
+++ viral-vtubers-frontend/src/app/main-page/main-page.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import {
+  ProductBlurbFragmentFragment,
+  ProductPaginationFragmentFragment,
+} from 'src/schema/type';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent implements OnInit {
+  products$?: Observable<ProductBlurbFragmentFragment[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router, private productService: ProductService) {
+    this.products$ = this.productService
+      .getProducts(undefined, undefined, undefined, 6)
+      .products$.pipe(map((p) => p.edges.map((e) => e.node)));
   }
 
+  ngOnInit(): void {}
 }
