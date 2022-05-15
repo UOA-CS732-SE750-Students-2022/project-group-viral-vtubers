f90c52bcf7a148c8cee7c1c99966d21a3211a270
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-05-15 22:51:33 +1200

    dynamically change docuemnt title

--- viral-vtubers-frontend/src/app/app.component.ts
+++ viral-vtubers-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { Router, RouterEvent } from '@angular/router';
 
 import { AuthService } from './shared/auth/auth.service';
@@ -10,15 +11,33 @@ import { AuthService } from './shared/auth/auth.service';
 })
 export class AppComponent {
   currentRoute = '';
+  defaultTitle = 'Viral Vtubers';
 
-  constructor(private router: Router, private authService: AuthService) {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private titleService: Title
+  ) {
     router.events.subscribe((event) => {
       if (event instanceof RouterEvent) {
         this.currentRoute = event.url.split('/')[1];
         this.currentRoute = this.currentRoute.split('?')[0];
         authService.getToken();
+
+        if (this.currentRoute) {
+          this.setTitle(
+            this.currentRoute.charAt(0).toUpperCase() +
+              this.currentRoute.slice(1)
+          );
+        } else {
+          this.setTitle(this.defaultTitle);
+        }
       }
     });
   }
   title = 'viral-vtubers-frontend';
+
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
 }
