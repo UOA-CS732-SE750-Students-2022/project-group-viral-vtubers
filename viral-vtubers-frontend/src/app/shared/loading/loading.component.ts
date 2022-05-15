fb04503173fc95f4b9891af42db00fd96de3a738
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-05-15 14:37:13 +1200

    rename chibi pngs and add loading component

---
+++ viral-vtubers-frontend/src/app/shared/loading/loading.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.component.scss'],
+})
+export class LoadingComponent implements OnInit {
+  @Input()
+  chibi: String = 'vivi';
+
+  @Input()
+  text: String = 'Loading...';
+
+  @Input()
+  isFlipped: boolean = false;
+
+  @Input()
+  height: string = '300px';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
