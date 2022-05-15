4c30dc1c033e7e9d158423d347bf71823f250559
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-05-15 15:15:26 +1200

    make it possible to have nested html inside loading

--- viral-vtubers-frontend/src/app/shared/loading/loading.component.ts
+++ viral-vtubers-frontend/src/app/shared/loading/loading.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class LoadingComponent implements OnInit {
   @Input()
